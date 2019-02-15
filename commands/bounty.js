8d3a86717d368ea446c1aec81d9862073407dba3
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2019-02-15 07:24:30 -0700

    bug fix
    changed spoilsRemind to bountyRemind.

--- commands/bounty.js
+++ commands/bounty.js
@@ -9,13 +9,13 @@ module.exports.run = async (client, message, args) => {
 
     message.channel.send(`${bountyEmote} I'll remind you that in \`24 hours\`.`);
 
-    const spoilsRemind = new Promise(function(resolve, reject) {
+    const bountyRemind = new Promise(function(resolve, reject) {
         setTimeout(function() {
           message.author.send('Your daily bounty is now available at the \`castle\`.');
         }, ms('24h'));
       });
       
-      spoilsRemind.then(function(value) {
+      bountyRemind.then(function(value) {
 
       });
 };

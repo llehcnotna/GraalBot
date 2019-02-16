5e8364f0c030ea8c40b136a8069df440eb56e4a7
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2019-02-16 14:10:29 -0700

    test command

--- commands/test.js
+++ commands/test.js
@@ -1,1 +1,13 @@
+const Discord = require('discord.js');
+const ms = require('ms');
 
+exports.run = async (client, message, args) => {
+  
+        setTimeout(function() {
+          message.author.send('test test 123');
+        }, ms('2m'));
+};
+
+module.exports.help = {
+    name: 'test'
+};

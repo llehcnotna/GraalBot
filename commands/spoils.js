d98baf22bf1ef7e2b4f6b7fec8ab7c689a038e6b
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2019-02-16 14:26:09 -0700

    bug fix
    cleaned up code.

--- commands/spoils.js
+++ commands/spoils.js
@@ -9,15 +9,11 @@ module.exports.run = async (client, message, args) => {
 
     message.channel.send(`${spoilsEmote} I'll remind you that in \`12 hours\`.`);
 
-    const spoilsRemind = new Promise(function(resolve, reject) {
         setTimeout(function() {
           message.author.send('You can now collect spoils from the \`giant blob\` again.');
+            console.log(`[ LOG ] I have sent a reminder to ${message.author}`);
         }, ms('12h'));
-      });
-      
-      spoilsRemind.then(function(value) {
 
-      });
 };
 
 module.exports.help = {

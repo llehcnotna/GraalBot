3923d5aba96f33d85751a58f51ab83c18b8c7bbe
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2019-02-16 14:57:02 -0700

    Added Try and Catach

--- commands/spoils.js
+++ commands/spoils.js
@@ -8,12 +8,15 @@ module.exports.run = async (client, message, args) => {
     const spoilsEmote = client.emojis.find(e => e.name === 'spoils');
 
     message.channel.send(`${spoilsEmote} I'll remind you that in \`12 hours\`.`);
-
+try {
         setTimeout(function() {
           message.author.send('You can now collect spoils from the \`giant blob\` again.');
             console.log(`[ LOG ] I have sent a reminder to ${message.author}`);
         }, ms('12h'));
-
+}
+    catch(error) {
+        console.log(error);
+}
 };
 
 module.exports.help = {

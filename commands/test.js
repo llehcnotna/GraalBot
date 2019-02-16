ae14b7aefc89b82b9c60178e0ab87ec1e2227dc0
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2019-02-16 14:16:29 -0700

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,12 @@ const ms = require('ms');
 
 exports.run = async (client, message, args) => {
   
+          message.channel.send('I\'ll send you a test!');
+  
         setTimeout(function() {
           message.author.send('test test 123');
         }, ms('2m'));
+        console.log(`[ LOG ] I have sent a reminder to ${message.author}`);
 };
 
 module.exports.help = {

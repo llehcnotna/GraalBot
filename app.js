e614b29b1e73c5c270c017379eb371a06908e5cf
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2020-03-10 15:16:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -66,32 +66,4 @@ client.on('message', async message => {
 
 });
 
-// TEST //
-var http = require('http'); //importing http
-
-function startKeepAlive() {
-    setInterval(function() {
-        var options = {
-            host: 'graalbot.herokuapp.com',
-            port: 80,
-            path: '/'
-        };
-        http.get(options, function(res) {
-            res.on('data', function(chunk) {
-                try {
-                    // optional logging... disable after it's working
-                    console.log("HEROKU RESPONSE: " + chunk);
-                } catch (err) {
-                    console.log(err.message);
-                }
-            });
-        }).on('error', function(err) {
-            console.log("Error: " + err.message);
-        });
-    }, 20 * 60 * 1000); // load every 20 minutes
-}
-
-startKeepAlive();
-//////////
-
 client.login(process.env.BOT_TOKEN);

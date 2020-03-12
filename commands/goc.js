acd463a436e9dc77ce12400fc472cfa91b6f57e5
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2020-03-12 17:04:00 -0600

    Create goc.js

---
+++ commands/goc.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    //>goc
+
+    let gocMember = message.guild.member(message.author);
+    let gocRole = message.guild.roles.find(`name`, "GraalClassic");
+    if(!gocRole) return message.reply("I couldn't find that role.");
+
+    message.delete();
+
+    if(!gocMember.roles.has(gocRole.id)) {
+        await(gocMember.addRole(gocRole.id));
+    
+        return message.channel.send(`**${message.author.username}**, you will now recive updates for **${gocRole.name}**!`);
+    }
+    else {
+        await(gocMember.removeRole(gocRole.id));
+    
+        return message.channel.send(`**${message.author.username}**, you will no longer recive updates for **${gocRole.name}**!`);
+    }
+}
+    
+
+  module.exports.help = {
+    name: "goc",
+    aliases: ['']
+  }

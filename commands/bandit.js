784f2c2c4ca8c20f51ee43acfdfd12ab8d936572
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2020-03-17 17:37:54 -0600

    Update bandit.js
    - full rework maybe it'll work.

--- commands/bandit.js
+++ commands/bandit.js
@@ -1,22 +1,23 @@
 const Discord = require('discord.js');
-const ms = require('ms');
+const db = require('quick.db');
+const moment = require('moment');
+require('moment-duration-format');
 
 module.exports.run = async (client, message, args) => {
 
     // >bandit
 
-    return message.channel.send('This command is not active.');
-
     let guildMember = message.member;
-    const banditRole = message.guild.roles.find(r => r.name === 'Bandit');
-    const banditEmote = client.emojis.find(e => e.name === 'bandit');
 
-    if(!banditRole) return message.channel.reply('I couldn\'t find a role called \`Bandit\`, please create one or ask a server administrator.');
+    let banditTimeout = 28800000;
+    let banditTime = db.fetch(`cooldown_${message.author.id}`);
+
+    const banditEmote = client.emojis.find(e => e.name === 'bandit');
 
     // Error Embed (active reminder)
     const banditError = new Discord.RichEmbed()
     .setColor('#b70000')
-    .setDescription('You already have an active reminder for `Bandit`.');
+    .setDescription(`You already have an active reminder for `Bandit`.\nCome back in ${moment.duration(banditTimeout - Date.now() - banditTime)).format('h [hrs] m [mins] s [seconds]')}.`);
 
     // Initial reminder Embed.
     const banditEmbed = new Discord.RichEmbed()
@@ -29,28 +30,18 @@ module.exports.run = async (client, message, args) => {
     .setDescription('You can now steal from the \`bandits caves\` again!')
     .setFooter(`Bandit Reminder`);
 
-    // If member has 'Bandit' role, don't send another reminder.
-    if(message.member.roles.some(r => r.name === 'Bandit'))
-    return message.channel.send(banditError);
-
-    // If member does not have 'bandit' role, add role and send prompt message.
-    await guildMember.addRole(banditRole.id);
-    message.channel.send(banditEmbed);
-
-    // DMed reminder.
-    try {
-        setTimeout(function() {
-            message.author.send(banditReminder);
-            message.member.removeRole(banditRole.id);
-                console.log(`[ LOG ] I have sent a bandit reminder to ${message.author.username}`);
-        }, ms('8h'));
+    if(banditTime !== null && banditTimeout - (Date.now() - banditTime) > 0) {
+        return message.channel.send(banditError);
+    } else {
+        db.set(`cooldown_${message.author.id}`, Date.now())
+        message.channel.send(banditEmbed)
     }
-    catch(error) {
-        console.log(error);
-    };
+    setTimeout(function() {
+        message.author.send(banditReminder)
+    }, banditTimeout);
 };
 
 module.exports.help = {
     name: 'bandit',
-    aliases: ['']
+    aliases: ['caves']
 };
